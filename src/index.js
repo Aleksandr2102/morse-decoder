aeb4afb76760d2b552c29d05d997fa5da29167cf
Aliaksandr Rebkavets <aleksschpak@gmail.com>
2020-03-22 21:50:05 +0300

    solution to morse-decoder

--- src/index.js
+++ src/index.js
@@ -39,6 +39,37 @@ const MORSE_TABLE = {
 
 function decode(expr) {
     // write your solution here
+let arr = [];
+let arr1 = [];
+for(let i = 0; i < expr.length; i += 10) {
+    arr.push(expr.slice(i, i + 10));
+    } 
+  
+for(let i = 0; i < arr.length; i++) {
+  arr[i] = arr[i].replace(/00/g, "");
+}
+  
+for(var i = 0; i < arr.length; i++) {
+    arr[i] = arr[i].replace(/10/g, ".");
+    }
+
+
+for(var i = 0; i < arr.length; i++) {
+    arr[i] = arr[i].replace(/11/g, "-");
+    }
+
+  
+for(var i = 0; i < arr.length; i++) {
+    arr[i] = arr[i].replace(/\*\*\*\*\*\*\*\*\*\*/g, " ")
+    for(let key in MORSE_TABLE) {
+        if (key === arr[i]) {
+            arr[i] = MORSE_TABLE[key]
+            }
+        }
+}
+  
+  let str = arr.join('');
+  return str;
 }
 
 module.exports = {
